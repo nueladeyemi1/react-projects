d5531450e265b54c91f901fd0eae9551398284f0
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-02-11 23:02:19 +0100

    birthday reminder

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+import './index.css'
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h2>{people.length} birthdays today</h2>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([])
+          }}
+        >
+          Clear all
+        </button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
